f41844c4839ed70506b4688fae1ef496adeb6186
S. M. Abrar Jahin <abrarjahin@live.com>
2017-09-21 17:00:42 +0600

    DB Created

---
+++ ApplicationManagement/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationManagement.DbModel;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    //List all tables here
+    public DbSet<Person> Person { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Filename=./applicationDB.db");
+    }
+}
---
+++ ApplicationManagement/DbModel/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApplicationManagement.DbModel
+{
+    public class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfbirth { get; set; }
+        public bool IsSelectedForExam { get; set; }
+    }
+}
